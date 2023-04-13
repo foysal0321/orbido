2260c3284d9ea1c626efdca8d3aeeb0d20f246ad
Foysal Hossain <foysalx4@gmail.com>
2023-04-13 16:40:21 +0600

    header-section-done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from './navbar/Navbar';
 import './Home.css'
 import Header from './header/Header';
+import About from './about/About';
 const Home = () => {
     return (
         <div className='main'>
             <Navbar />
             <Header />
+            <About />
         </div>
     );
 };
--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './About.css'
+import paypal from './Group 481710.png'
+import last from './Group 481722.png'
 
 const About = () => {
     return (
         <div>
-            
+            <div className="about flex flex-wrap justify-between">
+                <div className="about-img">               
+                    <img className='last' src={paypal}alt="" />
+                    <img className='paypal' src={last}alt="" />
+                </div>
+                <div className="about-text">
+                    <h2>Easily control your <br /> billing & invoicing.</h2>
+                    <p>Elit enim sed massa etiam. Mauris eu adipiscing <br /> ultrices ametodio aenean neque. Fusce ipsum orci <br /> rhoncus aliporttitor integer platea placerat.</p>
+                </div>
+            </div>
         </div>
     );
 };
